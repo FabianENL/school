af1bb245b622734d90ed0c75e1b4790a0a336efc
Fabian Eppens <f.eppens@icloud.com>
2024-10-08 16:50:42 +0200

    week5 programmeren basis

--- project digitaal/basis programmeren/repos/week5/opdracht7/main.c
+++ project digitaal/basis programmeren/repos/week5/opdracht7/main.c
@@ -2,6 +2,15 @@
 
 #include <stdio.h>
 
+void printArray(int* arr, int length) {
+	for (int i = 0; i < length; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+
+
 int isSchrikkel(int jaar) {
 	if ((jaar % 4 == 0 && !(jaar % 100 == 0)) || jaar % 400 == 0) {
 		return 1;
@@ -12,27 +21,55 @@ int isSchrikkel(int jaar) {
 }
 
 int schrikkelDagen(int d1, int m1, int y1, int d2, int m2, int y2) {
-	int count = 0;
+	int schrikkeldagen = 0;
 
-	if ((d1 <= 29 && m1 <= 2) || m1 < 2) {
-		count += 1;
-	}
-
-	if ((d2 >= 29 && m2 >= 2) || m2 > 2) {
-		count += 1;
-	}
-
-	for (int i = y1 + 1; i < y2 - 1; i++) {
-		if (isSchrikkel(i)) {
-			count += 1;
+	for (int jaar = y1; jaar <= y2; jaar++) {
+		if (isSchrikkel(jaar)) {
+			if ((jaar > y1 || (jaar == y1 && (m1 < 2 || (m1 == 2 && d1 <= 29)))) && (jaar < y2 || (jaar == y2 && (m2 > 2 || (m2 == 2 && d2 >= 29))))) {
+				schrikkeldagen++;
+			}
 		}
 	}
 
-	return count;
+	return schrikkeldagen;
+}
+
+
+int eersteWeekDagMaand(int m, int y) {
+	int dagen[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	if (isSchrikkel(y)) { dagen[1] = 29; }
+
+	int dagenInJaar = 0;
+	for (int i = 0; i < m - 1; i++) {
+		dagenInJaar += dagen[i];
+	}
+
+	int jarenOffset = y - 2000;
+	int dagenOffset = jarenOffset * 365 + schrikkelDagen(1, 1, 2000, 1, 1, y) + 5 + dagenInJaar;
+
+	return (dagenOffset % 7);
+}
+
+
+int vulMaandArray(int m, int y, int* maand) {
+	int maanden[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	if (isSchrikkel(y)) {
+		maanden[1] = 29;
+	}
+
+	for (int i = 0; i < maanden[m]; i++) {
+		maand[i] = i + 1;
+	}
+
+	
 }
 
 
 void main() {
-	int dagen = schrikkelDagen(1, 1, 2000, 3, 3, 2020);
-	printf("%d", dagen);
+	int maand[31] = {0};
+	vulMaandArray(1, 2024, maand);
+
+
+	printArray(maand, 31);
 }
